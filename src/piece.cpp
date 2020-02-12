98ca2bcd9842aa67a66fcd7bae6d94b1c0485d25
Piotr Brodzik <pbrodzik@mail.com>
2020-02-12 15:29:22 +0100

    king dev

--- src/piece.cpp
+++ src/piece.cpp
@@ -1,17 +1,30 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
 class Piece {
+private:
+	bool killed = false;
+	bool white = false;
 
 public:
-	virtual void show() = 0;
-
+	void setWhite(bool white) {
+		this->white = white;
+	}
+	void show() {
+		cout << white << endl;
+	}
 };
 
-public Derived: public Piece{
-	void show() { cout << "derived func called" << endl; }
+class King: public Piece {
+public:
+	King(bool white) {
+		this->setWhite(white);
+	}
 };
 
 int main(){
+	King k(false);
+	k.show();
+
 }
